3418a40deca3a5a54fde56b1442d09e048b358f4
jaime12-er <124049865@upq.edu.mx>
2025-09-19 21:04:53 -0600

    ejercicio2
    //declare un cuadrado como constante y le asigne la operacion con el metodo de la flecha

---
+++ practica no.1/ejercicio2.js
@@ -0,0 +1,6 @@
+
+const cuadrado = (numero) => {return numero * numero}; //declare un cuadrado como constante y le asigne la operacion con el metodo de la flecha 
+
+console.log(cuadrado(3));
+console.log(cuadrado(9));
+console.log(cuadrado(11));

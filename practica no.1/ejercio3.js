76f0b634b531dae0b469eab0ef25201b5e369f16
jaime12-er <124049865@upq.edu.mx>
2025-09-19 21:05:19 -0600

    ejercicio3
    //cree un arrrow de tipo constante y le asigne las variables nombre y edad y el final imprimi

---
+++ practica no.1/ejercio3.js
@@ -0,0 +1,6 @@
+
+const saludoPersonalizado = (nombre, edad ) => "Hola me llamo"+ nombre +"y tengo "+ edad +"años";
+
+console.log(saludoPersonalizado("jaime", 24));
+
+//cree un arrrow de tipo constante y le asigne las variables nombre y edad y el final imprimi 

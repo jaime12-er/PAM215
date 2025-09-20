8afdffad6833289adacf5ed9b6760a8909cdf0a2
jaime12-er <124049865@upq.edu.mx>
2025-09-19 21:04:14 -0600

    ejercicio1
    //coloque let porque ese valor despues lo voy a cambiar
    //coloque cosnst en edad porque esta informacion ya no cambia
    //tambien esta no cambia
    //mustra el mensaje en consola

---
+++ practica no.1/ejercicio1.js
@@ -0,0 +1,9 @@
+
+let nombre = "Armando"; //coloque let porque ese valor despues lo voy a cambiar 
+const edad = 25; //coloque cosnst en edad porque esta informacion ya no cambia 
+nombre  = "Ana Maria"; //tambien esta no cambia
+
+const saludo = "Hola," + nombre + ". Tienes" + edad + " años.";//utilize const para mostrar el mensaje del saludo 
+
+console.log(saludo);//mustra el mensaje en consola 
+

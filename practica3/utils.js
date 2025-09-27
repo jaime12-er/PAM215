3050a51dee92d68b36bc3ca2a34fde978aa267cf
jaime12-er <124049865@upq.edu.mx>
2025-09-26 23:57:17 -0600

    utils.js
    lo que hice fue crea una funcion que recive dos valores y los resto donde el export defaul es para utilizar esa funcion en otro archivo si lo quiero importar

---
+++ practica3/utils.js
@@ -0,0 +1,5 @@
+
+export default function restar (a,b){//lo que hice fue crea una funcion que recive dos valores y los resto
+// donde el export defaul es para utilizar esa funcion en otro archivo si lo quiero importar
+    return a - b;
+}

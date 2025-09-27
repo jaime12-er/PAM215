361d929c366984ba7c00365e130c8fc6b817b408
jaime12-er <124049865@upq.edu.mx>
2025-09-26 23:55:59 -0600

    main.js
     lo que estoy haciendo es traer la funcion restar que tengo en /otro archiivo que se llama utils.js depues voy a colocar  diferentes numeros para en console.log para verificar que si los reste

---
+++ practica3/main.js
@@ -0,0 +1,5 @@
+
+import restar from './utils.js';// lo que estoy haciendo es traer la funcion restar que tengo en 
+console.log("resultado", restar(25,8));//otro archiivo que se llama utils.js depues voy a colocar  
+console.log(restar(20,15));// diferentes numeros para en console.log para verificar que si los reste 
+console.log(restar(24,4));

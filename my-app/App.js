32ecfb1b562783e8e79e05b36213815b27878c89
jaime12-er <124049865@upq.edu.mx>
2025-10-13 20:43:56 -0600

    practica 6
    6 StyleSheet & Flex

--- my-app/App.js
+++ my-app/App.js
@@ -1,39 +1,14 @@
-//1. imports: zona de la importaciones
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Button} from 'react-native';
-import React, {useState} from 'react';
 
-//2. main : zona de componentes 
+import ContadorScreen from './screens/ContadorScreens';
+
 export default function App() {
 
 
-  const[contador ,setContador]=useState(0);
-
   return (
 
-    <View style={styles.container}>
-
-      <Text>Contador:{contador} </Text>
-    <Button title='Incrementar' onPress={()=>setContador(contador+1)}> </Button>
-    <Button title='Quitar' onPress={()=>setContador(contador-1)}> </Button>
-     <Button title='reiniciar' onPress={()=>setContador(contador-contador)}> </Button>
+    <ContadorScreen></ContadorScreen>
 
 
-
-      <StatusBar style="auto" />
-
-    </View>
-
-
+ 
   );
 }
-
-//3. estilos: zona de los estilos y posicionamiento
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

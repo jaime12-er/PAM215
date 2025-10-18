63b42d5217f1d23d0b4a36483d16de6532386931
jaime12-er <124049865@upq.edu.mx>
2025-10-17 20:25:47 -0600

    Práctica No.8: Buttons & Switch

--- my-app/screens/Botonesscreens.js
+++ my-app/screens/Botonesscreens.js
@@ -1,14 +1,50 @@
 
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Button, Switch } from 'react-native'
+import React, {useState} from 'react';
 
 export default function Botonesscreens () {
- 
+    const [esEncendido, cambiarEncendido] = useState(false);
+    const [color, cambiarColor] = useState('yellow');
+
     return (
-      <View>
-        <Text> proximo en presentar eq. Daniel Jr   </Text>
-      </View>
-    )
-  }
+        <View style={styles.container}>
+            <Text style={styles.text}>Control de Luz</Text>
+            {/* Controlador ternario */}
+            <Text style={[styles.text, { color: esEncendido ? color : 'black' }]}>
+                {esEncendido ? 'Luz encendida' : 'Luz apagada'}
+            </Text>
+            <Switch
+                value={esEncendido}
+                onValueChange={() => cambiarEncendido(!esEncendido)}
+                trackColor={{ false: 'gray', true: 'yellow' }}
+            />
+
+            <View style={styles.cajaBotones}>
+                <Button title={'Amarillo'} color={'#b9a100'}  onPress={() => esEncendido && cambiarColor('yellow')} />
+                <Button title={'Azul'} color={'blue'}  onPress={() =>esEncendido && cambiarColor('blue')} />
+                <Button title={'Rojo'} color={'red'} onPress={() =>esEncendido && cambiarColor('red')} />
+            </View>
+        </View>
+    );
+}
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+   container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#5d5d5d',
+    },
+    text: {
+        fontSize: 30,
+        marginBottom: 10,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    cajaBotones: {
+       flexDirection: 'row',
+        gap: 10,
+        marginTop: 10,
+    },
+});
